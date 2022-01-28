695655939048bb4702d0411986151d9d20e50c88
xovest <79008917+xovest@users.noreply.github.com>
2022-01-28 16:29:46 +0200

    got some basic features from the sc

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,6 +1,39 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  string public name = "Instachain";
+  string public name;
+  constructor() public {
+    name = "Decentragram";
+  }
   
+  mapping(uint => Image) public images;
+  uint public imageCount = 0;
+  
+  //storing
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+
+  //creating
+  event ImageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  //tipping
+  event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
 }
